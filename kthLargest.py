d6936edcc2fe2f9c748e751f553fe2e4f1bb9aa3
swechha <swechha16.05@gmail.com>
2018-04-30 17:03:45 +0530

    4th commit

--- kthLargest.py
+++ kthLargest.py
@@ -1,4 +1,4 @@
-#kth largest element 
+#kth largest element means (length - k + 1)th smallest
 
 print "Enter list of numbers : "
 array = [int(x) for x in raw_input().split(" ")]
@@ -7,25 +7,24 @@ print "Enter k :"
 k = input()
 
 def partition(arr,l,h) :
-	pivot = arr[0]
-	length = h-l + 1
-	j = -1
+	pivot = arr[l]
+	j = l-1
 	i = l
-	for i in range(length):
+	for i in range(l,h+1):
 		if arr[i] <= pivot :
 			j += 1
 			arr[i],arr[j] = arr[j],arr[i]
 			
-	arr[0],arr[j] = arr[j],arr[0]
+	arr[l],arr[j] = arr[j],arr[l]
 	return j
 
 def kthLargest(arr,l,h,k):
 	m = partition(arr,l,h)
-	if h - m == k-1:
+	if m == k-1:
 		return arr[m]
-	elif h - m > k -1:
+	elif m < k-1:
 		return kthLargest(arr,m+1,h,k)
 	else :
 		return kthLargest(arr,l,m-1,k)
 
-print kthLargest(array,0,len(array)-1,k)
+print kthLargest(array,0,len(array)-1,len(array)-k + 1)
--- kthSmallest.py
+++ kthSmallest.py
@@ -1,4 +1,4 @@
-#kth largest element 
+#kth smallest element 
 
 print "Enter list of numbers : "
 array = [int(x) for x in raw_input().split(" ")]
@@ -7,16 +7,15 @@ print "Enter k :"
 k = input()
 
 def partition(arr,l,h) :
-	pivot = arr[0]
-	length = h-l + 1
-	j = -1
+	pivot = arr[l]
+	j = l-1
 	i = l
-	for i in range(length):
+	for i in range(l,h+1):
 		if arr[i] <= pivot :
 			j += 1
 			arr[i],arr[j] = arr[j],arr[i]
 			
-	arr[0],arr[j] = arr[j],arr[0]
+	arr[l],arr[j] = arr[j],arr[l]
 	return j
 
 def kthSmallest(arr,l,h,k):
